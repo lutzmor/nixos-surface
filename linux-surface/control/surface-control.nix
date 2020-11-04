82323d449b6171feaba87d91a0eb905cd09a61f4
Moritz Lutz <moritz.lutz@gmail.com>
2020-11-04 17:36:49 +0100

    Update surface-control.nix

--- linux-surface/control/surface-control.nix
+++ linux-surface/control/surface-control.nix
@@ -1,7 +1,7 @@
 {stdenv, fetchurl, rustPlatform}:
 rustPlatform.buildRustPackage rec {
   name = "surface-control-${version}";
-  version = "0.2.5-3";
+  version = "0.3.0-1";
   src = fetchurl {
     url = "https://github.com/qzed/linux-surface-control/archive/v${version}.tar.gz";
     sha256 = "0llimw0xjf2agr1m3f46c9db87wpxjpdm6300l6sgc28vs03cjkg";
